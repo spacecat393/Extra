ae1adaf2dce25ba4d3f85a779fc7879e6adbdda9
nali <jjcakecat@gmail.com>
2025-02-18 22:14:38 +0700

    clean

--- src/main/java/com/nali/extra/mixin/IMixinEntityLivingBase.java
+++ src/main/java/com/nali/extra/mixin/IMixinEntityLivingBase.java
@@ -1,12 +1,12 @@
-package com.nali.extra.mixin;
-
-import net.minecraft.entity.EntityLivingBase;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
-@Mixin(EntityLivingBase.class)
-public interface IMixinEntityLivingBase
-{
-	@Invoker("updateDistance")
-	float GOupdateDistance(float yaw, float pitch);
-}
+//package com.nali.extra.mixin;
+//
+//import net.minecraft.entity.EntityLivingBase;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.gen.Invoker;
+//
+//@Mixin(EntityLivingBase.class)
+//public interface IMixinEntityLivingBase
+//{
+//	@Invoker("updateDistance")
+//	float GOupdateDistance(float yaw, float pitch);
+//}
